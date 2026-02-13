4c14e020562090ec2009673df791e17969e00d98
hamed <hamed.bensaad@protonmail.com>
2026-02-13 23:34:41 +0100

    add <leader>sgb keymap to search in git_bcommits

--- stow/.config/nvim/lua/plugins/telescope.lua
+++ stow/.config/nvim/lua/plugins/telescope.lua
@@ -1,3 +1,20 @@
+require("telescope").setup({
+	pickers = {
+		git_bcommits = {
+			git_command = {
+				"git",
+				"log",
+				"--decorate=short",
+				"--pretty=oneline",
+				"--abbrev-commit",
+				"--relative-date",
+				"--color=never",
+				"--",
+			},
+		},
+	},
+})
+
 return {
 	"nvim-telescope/telescope.nvim",
 	event = "VimEnter",
@@ -61,13 +78,17 @@ return {
 		vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
 		vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
 		vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+		vim.keymap.set("n", "<leader>sgf", builtin.git_files, { desc = "[S]earch [G]it [F]iles" })
+		vim.keymap.set("n", "<leader>sgc", builtin.git_commits, { desc = "[S]earch [G]it [C]ommits" })
+		vim.keymap.set("n", "<leader>sgs", builtin.git_stash, { desc = "[S]earch [G]it [S]tash" })
+		vim.keymap.set("n", "<leader>sgb", builtin.git_bcommits, { desc = "[S]earch [G]it [B]uffer commits" })
 		vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
 		vim.keymap.set("n", "<leader>su", builtin.grep_string, { desc = "[S]earch current [W]ord" })
 		vim.keymap.set("n", "<leader>f", builtin.live_grep, { desc = "[S]earch by [G]rep" })
 		vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
 		vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
 		vim.keymap.set("n", "<leader>st", builtin.colorscheme, { desc = "[S]earch [T]hemes" })
-		vim.keymap.set("n", "<leader>so", builtin.buffers, { desc = "[S] in [O]pen buffers" })
+		vim.keymap.set("n", "<leader>so", builtin.buffers, { desc = "[S]earch in [O]pen buffers" })
 
 		-- vim.keymap.set("v", "<leader>f", function()
 		-- 	local originalText = vim.getVisualSelection()
